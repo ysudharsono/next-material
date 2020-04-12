3ac732edc50ec989752879e124c1c24b4166f8a3
Yansen Sudharsono <yansen.sudharsono@gmail.com>
2020-04-12 20:27:14 +0700

    Example redux fetch from getinitprops and client side

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { getPosts } from '../lib/redux/actions/fooAction';
+
 const Index = (props) => {
   const { foo, custom } = props;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.getPosts();
+  };
+
   return (
     <div>
       <div>Prop from Redux {JSON.stringify(foo)}</div>
+      <button type="button" onClick={handleSubmit}>
+        Load
+      </button>
       <div>Prop from getInitialProps {custom}</div>
     </div>
   );
 };
 
 Index.getInitialProps = async ({ store, isServer, pathname, query }) => {
-  // component will be able to read from store's state when rendered
-  store.dispatch({ type: 'FOO', payload: 'foo from index' });
-  // you can pass some custom props to component from here
+  // await store.dispatch(getPosts());
   return { custom: 'custom' };
 };
 
-export default connect((state) => state)(Index);
+export default connect((state) => state, { getPosts })(Index);
